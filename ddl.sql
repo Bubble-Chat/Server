c7bbfdc7de1fca229851399f0f47c9adea9a2926
HINU0622 <HINU0622@gmail.com>
2023-09-08 12:10:42 +0900

    ddl 저장

---
+++ ddl.sql
@@ -0,0 +1,21 @@
+CREATE TABLE user(
+    email VARCHAR(150) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    photo_path VARCHAR(150),
+    PRIMARY KEY(email)
+);
+
+CREATE TABLE friend(
+    idx INT(5) NOT NULL AUTO_INCREMENT,
+    email VARCHAR(150) NOT NULL,
+    friend_email VARCHAR(150) NOT NULL,
+    PRIMARY KEY(idx),
+    CONSTRAINT UNIQUE_EMAIL_FRIENDEMAIL UNIQUE (email, friend_email)
+);
+
+CREATE TABLE room(
+    idx INT(5) NOT NULL AUTO_INCREMENT,
+    room_name VARCHAR(50) NOT NULL,
+    people INT(5) NOT NULL DEFAULT 0,
+    PRIMARY KEY(idx)
+);
