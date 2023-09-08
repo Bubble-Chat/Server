39f69a39bebea163fb3443f4b31ef2a1462f34f4
HINU0622 <HINU0622@gmail.com>
2023-09-08 11:44:32 +0900

    Client 에서 Server 접속 기능 구현

---
+++ src/main/java/kr/hs/dgsw/bubblechat/apiServer/socketIO/Message.java
@@ -0,0 +1,22 @@
+package kr.hs.dgsw.bubblechat.apiServer.socketIO;
+
+import lombok.Data;
+
+import java.awt.*;
+
+@Data
+public class Message {
+
+    private MessageType type;
+    private String message;
+    private String room;
+
+    public Message() { }
+
+    public Message(MessageType type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+}
+
---
+++ src/main/java/kr/hs/dgsw/bubblechat/apiServer/socketIO/MessageType.java
@@ -0,0 +1,5 @@
+package kr.hs.dgsw.bubblechat.apiServer.socketIO;
+
+public enum MessageType {
+    SERVER, CLIENT
+}
