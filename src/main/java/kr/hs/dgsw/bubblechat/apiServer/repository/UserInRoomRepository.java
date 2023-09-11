495b67e92719b6bf465e087c07393d7369e44f8f
HINU0622 <HINU0622@gmail.com>
2023-09-11 11:53:40 +0900

    유저의 소속 정보 구현

---
+++ src/main/java/kr/hs/dgsw/bubblechat/apiServer/repository/UserInRoomRepository.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.bubblechat.apiServer.repository;
+
+import kr.hs.dgsw.bubblechat.apiServer.entity.UserInRoomEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserInRoomRepository extends JpaRepository<UserInRoomEntity, Long> {
+
+    Optional<UserInRoomEntity> findByEmailAndRoomIdx(String email, Long roomIdx);
+
+}
