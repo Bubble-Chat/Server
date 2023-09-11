5c1c6b9bc9e693135e15f2749c1aa6a4ddc257ce
HINU0622 <HINU0622@gmail.com>
2023-09-11 11:54:08 +0900

    채팅 방 예외 처리

--- src/main/java/kr/hs/dgsw/bubblechat/apiServer/socketIO/SocketModule.java
+++ src/main/java/kr/hs/dgsw/bubblechat/apiServer/socketIO/SocketModule.java
@@ -4,8 +4,12 @@ import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.listener.ConnectListener;
 import com.corundumstudio.socketio.listener.DataListener;
 import com.corundumstudio.socketio.listener.DisconnectListener;
+import kr.hs.dgsw.bubblechat.apiServer.security.JwtUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 @Slf4j
@@ -35,8 +39,17 @@ public class SocketModule {
     private ConnectListener onConnected() {
         return (client) -> {
             String room = client.getHandshakeData().getSingleUrlParam("room");
-            client.joinRoom(room);
-            log.info("Socket ID[{}]  Connected to socket", client.getSessionId().toString());
+            String email = JwtUtils.getEmailByJwt(client.getHandshakeData().getSingleUrlParam("token"));
+            if(!socketService.isExists(room)) {
+                client.disconnect();
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "그런 방은 읎다.");
+            } else if(!socketService.isInTheRoom(email, room)) {
+                client.disconnect();
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "이 방에 입장되어있지 않습니다!");
+            } else {
+                client.joinRoom(room);
+                log.info("Socket ID[{}]  Connected to socket", client.getSessionId().toString());
+            }
         };
     }
 
--- src/main/java/kr/hs/dgsw/bubblechat/apiServer/socketIO/SocketService.java
+++ src/main/java/kr/hs/dgsw/bubblechat/apiServer/socketIO/SocketService.java
@@ -1,16 +1,34 @@
 package kr.hs.dgsw.bubblechat.apiServer.socketIO;
 
 import com.corundumstudio.socketio.SocketIOClient;
+import kr.hs.dgsw.bubblechat.apiServer.service.RoomService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service("socketService")
+@RequiredArgsConstructor
 @Slf4j
 public class SocketService {
 
-    public void sendMessage(String room, String eventName, SocketIOClient senderClient, String email, String message) {
+    private final RoomService roomService;
+
+    public boolean isInTheRoom(String email, String roomIdx) {
+        return roomService.isInTheRoom(email, Long.parseLong(roomIdx));
+    }
+
+    public boolean isExists(String roomIdx) {
+        return roomService.isExists(Long.parseLong(roomIdx));
+    }
+
+    public void sendMessage(String roomIdx, String eventName, SocketIOClient senderClient, String email, String message) {
+        if(!isInTheRoom(email, roomIdx)) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "이 방에 입장되어있지 않습니다!");
+        }
         for (
-                SocketIOClient client : senderClient.getNamespace().getRoomOperations(room).getClients()) {
+                SocketIOClient client : senderClient.getNamespace().getRoomOperations(roomIdx).getClients()) {
             if(!client.getSessionId().equals(senderClient.getSessionId())) {
                 client.sendEvent(eventName, new Message(MessageType.SERVER, email, message));
             }
