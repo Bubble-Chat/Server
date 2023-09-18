904482d945ceb5b4c68064924dcc8620c47c75c8
HINU0622 <HINU0622@gmail.com>
2023-09-18 13:34:32 +0900

    데이터 요청 방식 변경

--- src/main/java/kr/hs/dgsw/bubblechat/apiServer/controller/UserApiController.java
+++ src/main/java/kr/hs/dgsw/bubblechat/apiServer/controller/UserApiController.java
@@ -24,19 +24,17 @@ public class UserApiController {
 
     @PostMapping("/signin")
     public ResponseEntity<AuthUser> getUserData(HttpServletRequest request,
-                                                @RequestParam(name = "provider") String provider,
-                                                @RequestParam(name = "token") String token
-                                                /*00@RequestBody Token token*/) {
+                                                @RequestBody Token token) {
 
         // Provider에게 token을 이용해서 이메일을 얻는다.
 
         // 데이터베이스에 이메일이 등록되어 있다면 사용자 정보와 JWT을 만들어서 보내준다.
         // 없으면 error 메시지를 보낸다.
 
-        log.info("Provider {}", provider);
-        log.info("Token {}", token);
+        log.info("Provider {}", token.getProvider());
+        log.info("Token {}", token.getToken());
 
-        AuthUser authUser = userService.getUserByToken(provider, token);
+        AuthUser authUser = userService.getUserByToken(token.getProvider(), token.getToken());
 
         return ResponseEntity.ok(authUser);
 
