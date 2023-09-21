7ec3d2f8eaa477f5bac348e345c91a17aa9fcfaf
HINU0622 <HINU0622@gmail.com>
2023-09-21 20:54:16 +0900

    요청 방식 수정

--- src/main/java/kr/hs/dgsw/bubblechat/apiServer/controller/UserApiController.java
+++ src/main/java/kr/hs/dgsw/bubblechat/apiServer/controller/UserApiController.java
@@ -24,20 +24,22 @@ public class UserApiController {
 
     @PostMapping("/signin")
     public ResponseEntity<AuthUser> getUserData(HttpServletRequest request,
-                                                @RequestBody Token token) {
+                                                @RequestBody Object o) {
 
         // Provider에게 token을 이용해서 이메일을 얻는다.
 
         // 데이터베이스에 이메일이 등록되어 있다면 사용자 정보와 JWT을 만들어서 보내준다.
         // 없으면 error 메시지를 보낸다.
 
-        log.info("Provider {}", token.getProvider());
-        log.info("Token {}", token.getToken());
+        log.info("Object {}", o);
 
-        AuthUser authUser = userService.getUserByToken(token.getProvider(), token.getToken());
+//        log.info("Provider {}", token.getProvider());
+//        log.info("Token {}", token.getToken());
 
-        return ResponseEntity.ok(authUser);
+//        AuthUser authUser = userService.getUserByToken(token.getProvider(), token.getToken());
 
+//        return ResponseEntity.ok(authUser);
+        return null;
     }
 
     @GetMapping("/search")
